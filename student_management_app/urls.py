7bf7133a871aa9cf52e0aaad67db8b5ba4a0e639
mehfooz-analyst5 <analyst5@ugspk.com>
2024-01-30 18:03:29 +0500

    staff user added

--- student_management_app/urls.py
+++ student_management_app/urls.py
@@ -6,7 +6,13 @@ from . import views
 
 urlpatterns = [
 
-     path('', views.ShowPage, name='home')
+     path('', views.ShowPage, name='home'),
+     path('login/', views.ShowLoginPage, name='login'),
+     path('logout/', views.Logoutpage, name='logout'),
 
+     #-------------------- HOD Views ---------------
+
+     path('hod-admin/', views.HodPageView, name='hod-admin'),
+     path('add-staff/', views.addStaff, name='add-staff'),
 
 ] 
